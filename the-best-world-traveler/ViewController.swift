1268d0a4027b1979c25bce6945172d16618144f1
Avery Brown <averymbrown22@gmail.com>
2021-04-01 00:24:19 -0400

    storyboard changes, added user location to mapView

--- the-best-world-traveler/ViewController.swift
+++ the-best-world-traveler/ViewController.swift
@@ -6,16 +6,56 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
+        locationManager.delegate = self
+
         // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
     }
 
-
+    
+    // MARK: CoreLocation Methods
+        
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            print("App is authorized")
+            locationManager.startUpdatingLocation()
+        }
+        
+        if status == .notDetermined || status == .denied {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+        
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //print("Location \(locations.first)")
+        print("Latitude = \(locations.first!.coordinate.latitude)")
+        print("Longitude = \(locations.first!.coordinate.longitude)")
+        
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+                let region = MKCoordinateRegion(center: location.coordinate, span: span)
+                mapView.setRegion(region, animated: true)
+            }
+        
+        locationManager.stopUpdatingLocation()
+        
+    }
 }
 
